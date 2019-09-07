7b234bb993c78b0bc3597098db44223c39c6e4df
lester wei <lunyi.lester@gmail.com>
2019-09-07 22:06:54 +0800

    initial

---
+++ ImageResizer/ImageProcess.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageResizer
+{
+    public class ImageProcess
+    {
+
+        /// <summary>
+        /// 清空目的目錄下的所有檔案與目錄
+        /// </summary>
+        /// <param name="destPath">目錄路徑</param>
+        public void Clean(string destPath)
+        {
+            if (!Directory.Exists(destPath))
+            {
+                Directory.CreateDirectory(destPath);
+            }
+            else
+            {
+                var allImageFiles = Directory.GetFiles(destPath, "*", SearchOption.AllDirectories);
+
+                foreach (var item in allImageFiles)
+                {
+                    File.Delete(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 進行圖片的縮放作業
+        /// </summary>
+        /// <param name="sourcePath">圖片來源目錄路徑</param>
+        /// <param name="destPath">產生圖片目的目錄路徑</param>
+        /// <param name="scale">縮放比例</param>
+        public void ResizeImages(string sourcePath, string destPath, double scale)
+        {
+            var allFiles = FindImages(sourcePath);
+            foreach (var filePath in allFiles)
+            {
+                ProcessImage(filePath, destPath, scale);
+            }
+        }
+
+        public async Task ResizeImagesAsync(string sourcePath, string destPath, double scale)
+        {
+            var allFiles = FindImages(sourcePath);
+
+            var tasks = allFiles.Select(filePath => Task.Factory.StartNew(() => ProcessImageAsync(filePath, destPath, scale), TaskCreationOptions.LongRunning));
+            //var tasks = allFiles.Select(filePath => Task.Run(() => ProcessImageAsync(filePath, destPath, scale)));
+
+            await Task.WhenAll(tasks);
+        }
+
+
+        public void ProcessImage(string filePath, string destPath, double scale)
+        {
+            Image imgPhoto = Image.FromFile(filePath);
+            string imgName = Path.GetFileNameWithoutExtension(filePath);
+
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+
+            int destionatonWidth = (int)(sourceWidth * scale);
+            int destionatonHeight = (int)(sourceHeight * scale);
+
+            Bitmap processedImage = ProcessBitmap((Bitmap)imgPhoto,
+                sourceWidth, sourceHeight,
+                destionatonWidth, destionatonHeight);
+
+            string destFile = Path.Combine(destPath, imgName + ".jpg");
+            processedImage.Save(destFile, ImageFormat.Jpeg);
+        }
+
+        private async Task ProcessImageAsync(string filePath, string destPath, double scale)
+        {
+            Image imgPhoto = Image.FromFile(filePath);
+            string imgName = Path.GetFileNameWithoutExtension(filePath);
+
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+
+            int destionatonWidth = (int)(sourceWidth * scale);
+            int destionatonHeight = (int)(sourceHeight * scale);
+
+            Bitmap processedImage = await ProcessBitmapAsync((Bitmap)imgPhoto,
+                sourceWidth, sourceHeight,
+                destionatonWidth, destionatonHeight);
+
+            string destFile = Path.Combine(destPath, imgName + ".jpg");
+            processedImage.Save(destFile, ImageFormat.Jpeg);
+        }
+        /// <summary>
+        /// 找出指定目錄下的圖片
+        /// </summary>
+        /// <param name="srcPath">圖片來源目錄路徑</param>
+        /// <returns></returns>
+        public string[] FindImages(string srcPath)
+        {
+            return Directory.GetFiles(srcPath, "*.jpg", SearchOption.AllDirectories);
+        }
+
+        /// <summary>
+        /// 針對指定圖片進行縮放作業
+        /// </summary>
+        /// <param name="img">圖片來源</param>
+        /// <param name="srcWidth">原始寬度</param>
+        /// <param name="srcHeight">原始高度</param>
+        /// <param name="newWidth">新圖片的寬度</param>
+        /// <param name="newHeight">新圖片的高度</param>
+        /// <returns></returns>
+        private Task<Bitmap> ProcessBitmapAsync(Bitmap img, int srcWidth, int srcHeight, int newWidth, int newHeight)
+        {
+            return Task.Run(() =>
+            {
+                Bitmap resizedbitmap = new Bitmap(newWidth, newHeight);
+                using (Graphics g = Graphics.FromImage(resizedbitmap))
+                {
+                    g.InterpolationMode = InterpolationMode.High;
+                    g.SmoothingMode = SmoothingMode.HighQuality;
+                    g.Clear(Color.Transparent);
+                    g.DrawImage(img,
+                        new Rectangle(0, 0, newWidth, newHeight),
+                        new Rectangle(0, 0, srcWidth, srcHeight),
+                        GraphicsUnit.Pixel);
+                }
+                return resizedbitmap;
+            });
+        }
+        private Bitmap ProcessBitmap(Bitmap img, int srcWidth, int srcHeight, int newWidth, int newHeight)
+        {
+            Bitmap resizedbitmap = new Bitmap(newWidth, newHeight);
+            using (Graphics g = Graphics.FromImage(resizedbitmap))
+            {
+                g.InterpolationMode = InterpolationMode.High;
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.Clear(Color.Transparent);
+                g.DrawImage(img,
+                    new Rectangle(0, 0, newWidth, newHeight),
+                    new Rectangle(0, 0, srcWidth, srcHeight),
+                    GraphicsUnit.Pixel);
+            }
+            return resizedbitmap;
+        }
+    }
+}
---
+++ ImageResizer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageResizer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
+            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
+            string destinationPathAsync = Path.Combine(Environment.CurrentDirectory, "outputAsync"); ;
+
+            ImageProcess imageProcess = new ImageProcess();
+
+            var before = RunImages(imageProcess, destinationPath, sourcePath);
+            var after = await RunImagesAsync(imageProcess, destinationPathAsync, sourcePath);
+            var percent = 100 * (before - after) / before;
+
+            Console.WriteLine($"效能提升百分比: {percent} %");
+
+            Console.ReadKey();
+        }
+
+        private static long RunImages(ImageProcess imageProcess, string destinationPath, string sourcePath)
+        {
+            imageProcess.Clean(destinationPath);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+            sw.Stop();
+            var milliseconds = sw.ElapsedMilliseconds;
+            Console.WriteLine($"同步處理花費時間: {milliseconds} ms");
+            return milliseconds;
+        }
+
+        private static async Task<long> RunImagesAsync(ImageProcess imageProcess, string destinationPath, string sourcePath)
+        {
+            imageProcess.Clean(destinationPath);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            await imageProcess.ResizeImagesAsync(sourcePath, destinationPath, 2.0);
+            sw.Stop();
+            var milliseconds = sw.ElapsedMilliseconds;
+            Console.WriteLine($"非同步處理花費時間: {milliseconds} ms");
+            return milliseconds;
+        }
+    }
+}
